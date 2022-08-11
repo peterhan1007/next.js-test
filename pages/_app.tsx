6491525d2af685da07cc0dbdd355813d67253970
harry <harry@gmail.io>
2022-08-10 22:55:17 -0400

    refactored

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,9 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../chakra/theme";
 
-import "@fontsource/nunito/300.css";
-import "@fontsource/nunito/400.css";
-import "@fontsource/nunito/500.css";
-import "@fontsource/nunito/600.css";
-import "@fontsource/nunito/700.css";
-import "@fontsource/nunito/800.css";
-import "@fontsource/nunito/900.css";
-
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
-
   return (
     <ChakraProvider theme={theme}>
       <Layout>
