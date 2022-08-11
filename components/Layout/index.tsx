9778dde75d779799476de8b054180342bdcfaed4
harry <harry@gmail.io>
2022-08-11 00:00:37 -0400

    added: footer

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode, useEffect } from "react";
 
 import Header from "./Header";
+import Footer from "./footer";
 
 interface ChildrenProps {
   children: ReactNode;
@@ -12,6 +13,7 @@ const Layout: React.FC<ChildrenProps> = ({ children }) => {
     <div>
       <Header />
       {children}
+      <Footer />
     </div>
   );
 };
